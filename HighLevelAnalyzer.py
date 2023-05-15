954ea04e6f50c49a8b58564f37708e64d5e1a1e4
J R <Jackjan4@users.noreply.github.com>
2023-05-15 08:39:52 +0200

    Update HighLevelAnalyzer.py

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -167,6 +167,6 @@ class Hla(HighLevelAnalyzer):
                 self.following_type = 1
                 return AnalyzerFrame('read_rx_pl_wid', frame.start_time, frame.end_time)
 				
-			# NOP
-			if (mosi_int == 0b11111111):
-				return AnalyzerFrame('nop', frame.start_time, frame.end_time)
+	    # NOP
+	    if (mosi_int == 0b11111111):
+		return AnalyzerFrame('nop', frame.start_time, frame.end_time)
